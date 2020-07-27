7dd5bce191439493689d43785da60ebfcd31f5d3
Paulo Henrique Scherer <paulo@rocketlab.com.br>
2020-07-27 17:09:49 -0300

    Creates homepage using styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import data from './data/dados_iniciais.json';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world,</h1>
-      <h2>welcome to IdleFlix</h2>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+      <BannerMain
+        videoTitle={data.categorias[0].videos[0].titulo}
+        url={data.categorias[0].videos[0].url}
+        videoDescription={"O que é front-end? Trabalhando na área..."}
+      />
+      {
+        data.categorias.map((categoria) => (
+          <Carousel category={categoria} />
+        ))
+      }
+      <Footer />
     </div>
   );
 }
